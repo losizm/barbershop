eb7bd9c9be2e11aa17af672bd31c5153758e1083
Carlos Conyers <carlos.conyers@hotmail.com>
2021-11-28 04:46:19 -0500

    Refactored UI app

--- src/universal/ui/comments/app.js
+++ src/universal/ui/comments/app.js
@@ -13,15 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function() {
+(function initApp() {
   function loadComments() {
     let req = new XMLHttpRequest();
     req.open('GET', '/api/comments');
-    req.addEventListener('readystatechange', () => {
-      if (req.readyState == 4 && req.status == 200) {
-        let comments = JSON.parse(req.responseText);
-        replaceList(comments);
-      }
+    req.addEventListener('load', () => {
+      if (req.status >= 200 && req.status <= 299)
+        setCommentList(JSON.parse(req.responseText));
     });
     req.send(null);
   }
@@ -30,9 +28,9 @@
     let req = new XMLHttpRequest();
     req.open('POST', '/api/comments');
     req.setRequestHeader('Content-Type', 'text/plain; charset=UTF-8');
-    req.addEventListener('readystatechange', () => {
-      if (req.readyState == 4 && (req.status >= 200 && req.status <= 299)) {
-        clearComment();
+    req.addEventListener('load', () => {
+      if (req.status >= 200 && req.status <= 299) {
+        setComment('');
         loadComments();
       }
     });
@@ -42,67 +40,67 @@
   function deleteComment(id) {
     let req = new XMLHttpRequest();
     req.open('DELETE', `/api/comments/${id}`);
-    req.addEventListener('readystatechange', () => {
-      if (req.readyState == 4 && (req.status >= 200 && req.status <= 299)) {
-        clearComment();
+    req.addEventListener('load', () => {
+      if (req.status >= 200 && req.status <= 299)
         loadComments();
-      }
     });
     req.send(null);
   }
 
-  function clearComment() {
-    let input = document.querySelector('section.comments > form > input');
-    input.value = '';
+  function getComment() {
+    return document.querySelector('section.comments input').value;
   }
 
-  function replaceList(comments) {
-    let section = document.querySelector('section.comments')
-    let list = section.querySelector('ul');
+  function setComment(text) {
+    document.querySelector('section.comments input').value = text;
+  }
+
+  function setCommentList(comments) {
+    function createList(comments) {
+      let list = document.createElement('ul');
+      comments.map(comment => createListItem(comment))
+        .forEach(item => list.appendChild(item));
+
+      return list;
+    }
+
+    function createListItem(comment) {
+      let time = document.createElement('div');
+      time.appendChild(document.createTextNode(comment.time));
+      time.className = 'time';
+
+      let text = document.createElement('div');
+      text.appendChild(document.createTextNode(comment.text));
+      text.className = 'text';
+      text.title = 'Click to delete'
+      text.addEventListener('click', () => deleteComment(comment.id));
+
+      let listItem = document.createElement('li');
+      listItem.appendChild(time);
+      listItem.appendChild(text);
+
+      return listItem;
+    }
+
+    let section = document.querySelector('section.comments');
+    let list    = document.querySelector('section.comments ul');
     section.replaceChild(createList(comments), list);
   }
 
-  function createList(comments) {
-    let list = document.createElement('ul');
-    comments.map(comment => createListItem(comment))
-      .forEach(item => list.appendChild(item));
-
-    return list;
-  }
-
-  function createListItem(comment) {
-    let time = document.createElement('div');
-    time.appendChild(document.createTextNode(comment.time));
-    time.className = 'time';
-
-    let text = document.createElement('div');
-    text.appendChild(document.createTextNode(comment.text));
-    text.className = 'text';
-    text.title = 'Click to delete'
-    text.addEventListener('click', () => deleteComment(comment.id));
-
-    let listItem = document.createElement('li');
-    listItem.appendChild(time);
-    listItem.appendChild(text);
-
-    return listItem;
-  }
-
-  function addFormHandler() {
-    let form = document.querySelector('section.comments > form');
-    let input = form.querySelector('input');
+  function addCommentHandler() {
+    let form = document.querySelector('section.comments form');
 
     form.onsubmit = () => {
-      let value = input.value.trim();
-      input.value = value;
+      let text = getComment().trim();
+      setComment(text);
 
-      if (value !== '')
-        postComment(value);
+      if (text !== '')
+        postComment(text);
 
       return false;
     };
   }
 
-  addFormHandler();
+  addCommentHandler();
   loadComments();
-})()
+})();
